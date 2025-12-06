e45cea8c5ec193e64b3df5629b085b2ee526030f
Andrey Tropin <TropinAB@sibur.ru>
2025-12-06 11:49:17 +0300

    Add call GEO API

--- src/weather.js
+++ src/weather.js
@@ -10,13 +10,12 @@ export async function loadCurrentLocation(locationEl) {
   locationEl.append(labelLoading);
 
   // эмитация загрузки данных
-  await sleep(3000);
+  const response = await fetch("https://get.geojs.io/v1/ip/geo.json");
+  const data = await response.json();
 
   labelLoading.remove();
 
-  return {
-    city: "Санкт-Петербург",
-  };
+  return data;
 }
 
 export function displayLocationInfo(locationEl, location) {
