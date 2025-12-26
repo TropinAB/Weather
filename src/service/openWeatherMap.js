5a88568b7c38743fce781d4a0515093b90037f2a
Andrey Tropin <TropinAB@sibur.ru>
2025-12-26 20:25:00 +0300

    Add service/openWeatherMap and tests for it

---
+++ src/service/openWeatherMap.js
@@ -0,0 +1,45 @@
+const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_ID = [
+  "d",
+  "0",
+  "1",
+  "7",
+  "5",
+  "7",
+  "e",
+  "8",
+  "c",
+  "5",
+  "4",
+  "0",
+  "2",
+  "d",
+  "d",
+  "5",
+  "3",
+  "e",
+  "8",
+  "b",
+  "c",
+  "d",
+  "4",
+  "0",
+  "7",
+  "1",
+  "f",
+  "3",
+  "9",
+  "d",
+  "7",
+  "9",
+]
+  .reverse()
+  .join("");
+
+export async function getWeatherData(latitude, longitude) {
+  const url = `${WEATHER_URL}?units=metric&lat=${latitude}&lon=${longitude}&appid=${API_ID}&lang=ru`;
+  const response = await fetch(url);
+  if (response.ok) return await response.json();
+  // Вернуть ошибку
+  throw new Error(`Ошибка ${response.status}: ${response.statusText}`);
+}
