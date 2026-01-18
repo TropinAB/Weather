3b965dc2dba4df4e91161771e7af9dab392b89bb
Andrey Tropin <TropinAB@sibur.ru>
2026-01-18 18:27:38 +0300

    Add EventBus and tests for it

---
+++ src/service/EventBus.js
@@ -0,0 +1,34 @@
+function EventBus() {
+  const events = new Map();
+
+  /// очистить все события
+  this.clearEvents = function () {
+    events.clear();
+  };
+
+  /// добавить слушателя события
+  this.on = function (event, handler, allowDoubleHandlers = false) {
+    let handlers = events.get(event) || [];
+    if (!allowDoubleHandlers) {
+      handlers = handlers.filter((item) => item !== handler); // удалить обработчик, если он был добавлен ранее
+    }
+    handlers.push(handler);
+    events.set(event, handlers);
+  };
+
+  /// удалить слушателя события
+  this.off = function (event, handler) {
+    if (!events.has(event)) return;
+    const handlers = events.get(event).filter((item) => item !== handler);
+
+    events.set(event, handlers);
+  };
+
+  /// вызов события
+  this.trigger = function (event, data) {
+    if (!events.has(event)) return;
+    events.get(event).forEach((listener) => listener && listener(data));
+  };
+}
+
+export const eventBus = new EventBus();
