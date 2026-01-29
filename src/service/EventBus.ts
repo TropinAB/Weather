6537a56a9bea3eeeefd81aed3ebf1f265fceb555
Andrey Tropin <TropinAB@sibur.ru>
2026-01-29 16:26:20 +0300

    JS->TS EventBus

---
+++ src/service/EventBus.ts
@@ -0,0 +1,68 @@
+type Handler<Params extends unknown[] = []> = (...params: Params) => void;
+
+class EventBus {
+  private events: Map<string, Handler[]> = new Map();
+  private eventsTimerId: Map<string, number> = new Map();
+
+  constructor() {}
+
+  /// очистить все события
+  clearEvents(): void {
+    this.events && this.events.clear();
+  }
+
+  /// добавить слушателя события
+  on<Params extends unknown[]>(
+    event: string,
+    handler: Handler<Params>,
+    allowDoubleHandlers: boolean = false,
+  ): void {
+    let handlers: Handler[] = this.events.get(event) || [];
+    if (allowDoubleHandlers || handlers.indexOf(handler) === -1) {
+      handlers.push(handler); // разрешены дубли или нет обработчика - добавить
+    }
+    this.events.set(event, handlers);
+  }
+
+  /// удалить слушателя события
+  off<Params extends unknown[]>(event: string, handler: Handler<Params>) {
+    if (!this.events.has(event)) return;
+    const handlers: Handler[] = (this.events.get(event) || []).filter(
+      (item: Handler) => item !== handler,
+    );
+    this.events.set(event, handlers);
+  }
+
+  /// вызов события
+  trigger<Params extends unknown[]>(event: string, ...data: Params) {
+    if (this.events.has(event)) {
+      (this.events.get(event) || []).forEach(
+        (handler: Handler<Params>) =>
+          handler && setTimeout(() => handler(...data), 0),
+      );
+    }
+  }
+
+  triggerDebounced<Params extends unknown[]>(
+    timeout: number,
+    event: string,
+    ...data: Params
+  ) {
+    const content = this;
+    if (this.eventsTimerId.has(event))
+      clearTimeout(this.eventsTimerId.get(event));
+    this.eventsTimerId.set(
+      event,
+      setTimeout(() => {
+        this.eventsTimerId.delete(event);
+        content.trigger.call(content, event, ...data);
+      }, timeout),
+    );
+  }
+}
+
+export const eventBus: EventBus = new EventBus();
+
+function h(a: string, b: string): void {}
+eventBus.on("", h);
+eventBus.off("", h);
