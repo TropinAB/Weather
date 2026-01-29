b20376ebdace3deb0c79b2942ddb98f2663c7634
Andrey Tropin <TropinAB@sibur.ru>
2026-01-29 18:10:46 +0300

    JS->TS geoJS

---
+++ src/service/geoJS.ts
@@ -0,0 +1,36 @@
+import { eventBus } from "./EventBus.js";
+
+export const eventNameCall = "geo:getLocation";
+export const eventNameResult = "geo:loaded";
+export type GeoLocation = {
+  accuracy: number;
+  area_code: string;
+  asn: number;
+  city: string;
+  continent_code: string;
+  country: string;
+  country_code: string;
+  country_code3: string;
+  ip: string;
+  latitude: string;
+  longitude: string;
+  organization: string;
+  organization_name: string;
+  region: string;
+  timezone: string;
+};
+
+function requestCurrentLocationData(): void {
+  fetch("https://get.geojs.io/v1/ip/geo.json")
+    .then((response: Response) => {
+      if (!response.ok) {
+        throw new Error(`Ошибка ${response.status}: ${response.statusText}`);
+      }
+      return response.json(); // as Promise<GeoLocation>;
+    })
+    .then((result: GeoLocation) => eventBus.trigger(eventNameResult, result))
+    .catch((error: Error) => eventBus.trigger(eventNameResult, error));
+}
+
+/// зарегистрировать вызывающее событие
+eventBus.on(eventNameCall, requestCurrentLocationData);
